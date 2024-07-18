6f1f035ef9a4e81df18d0edcb0e9f8814405dce1
Dilip Sahoo <66472615+dilipkumarsahoo@users.noreply.github.com>
2024-07-18 20:34:46 +0530

    adding switch case program

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "april"
+
+switch (month) {
+    case "january":
+        console.log("january");
+        break;
+    case "fabuary":
+        console.log("fabuary");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("Non of this");
+        break;
+}
